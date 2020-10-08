1772caacfc0ff37dfff14d330e15fc148d967b08
Dawit Ayana <dawitz28@gmail.com>
2020-10-07 21:00:00 -0700

    changes to Js and CSS files

--- js/app.js
+++ js/app.js
@@ -51,48 +51,82 @@
 //         alert('Thank you for playing my About me game' + guestName + '.' + 'I hope you got to know me a bit.' + 'until next time take good care of yourself.');
 //     } 
 // }
+//Not using this 
+//for (var i = 0; i < 4; i++){
+//}
 
-// var i = 0;
-// var allowedGuesses = 4;
+//question # 6
+//  var i = 0;
+//  var allowedGuesses = 4;
+//  var correctGuesses = false;
 
-// //for (var i = 0; i < 4; i++){
-// //}
+// while (i < 3 && !correctGuesses) {
+//         i++;
+//      var guess = prompt('How many cars do you think I have?');
+//      console.log(typeof guess, guess);
+//      if (guess > allowedGuesses) {
+//          alert('That is too high. Please try again.');
+//      }else if (guess < allowedGuesses) {
+//          alert('That is too low. Please try again.');
+//      }else if (guess === 4) {
+//          alert('That is correct.');
+//          correctGuesses = true;
+//          break
+//      }
+     
+//      if (!correctGuesses) {
+//         alert('Sorry you have no more tries left. The correct answer is 4.');
+//      }
+ 
+var allowedGuesses = 4;
 
-// while (i < 4) {
-//     var guess = prompt('How many cars do you think I have?');
-//     if (guess > 3) {
-//         alert('That is too high. Please try again.');
-//     }
-//     else if (guess < 3) {
-//         alert('That is too low. Please try again.');
-//     }
-//     else if (guess = 3) {
-//         alert('That is correct.');
-//         break
-//     }
-//     i++
-//     if (i === allowedGuesses) {
-//         alert('Sorry you have no more tries left. The correct answer is 3.');
-//     }
-// }
+while (allowedGuesses > 0) {
+    var correctGuesses = prompt('How many cars do you think I have?');
+
+    if (correctGuesses === '5'){
+        alert('That is correct.');
+        break;
+    }
+    if (allowedGuesses === 1){
+        alert('No more attempt left. The correct answer is 5.');
+    }
+    if (correctGuesses > '5' && allowedGuesses > 1) {
+        alert(`That's too high, Please try again. ${allowedGuesses - 1} attempt(s) remaining.`);
+    }
+    if (correctGuesses < '5' && allowedGuesses > 1) {
+        alert(`That's too low, please try again. ${allowedGuesses - 1} attempt(s) remainig.`);
+    }
+    allowedGuesses--;
+}
+
+
+// question # 7
 
 //var drinks = ['Water', 'Apple Juice', 'Orange Juice', 'Beer', 'Vodka', 'Coke', 'Milk']
 
-var rightAnswers = ['Water', 'Milk', 'Coke'];
-var allowwedAttempts = 6;
+// for (var i = 0; i < 6; i++)
 
-var answer = prompt('What do you think my favorite drinks are?');
-for (var i = 0; i < rightAnswers.length; i++){
-    if (answer !== rightAnswers)[i];{
-        alert('That is incorrect answer. Please try again.')
-    } 
-    else if (){
-    
-    }
-    if (answer === rightAnswers[i])
-    result = true; 
-    alert('That is a correct answer.');
-}
+// var rightAnswers = ['Water', 'Milk', 'Coke'];
+// var mainQuestion = prompt('What do you think my favorite drinks are?');
+// //var allowwedAttempts = 6;
+
+
+//     if (mainQuestion !== rightAnswers)[i];{
+//         alert('That is incorrect answer. Please try again.');
+//     } 
+//     else if (mainQuestion === rightAnswers[0]){
+//         alert('Correct. That is the right answer.');
+//         break;
+//     }
+//     else if (mainQuestion === rightAnswers[1]){
+//         alert('Correct. That is the right answer.');
+//         break;
+//     }
+//     else if (mainQuestion === rightAnswers[2]){
+//         alert('Correct. That is the right answer.');
+//         break
+//     }
+
     
 
 
